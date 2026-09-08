3d92e94a3badf62134f8b08618840ce5274e3d4f
imkeykkk <2066896375@qq.com>
2026-09-08 17:12:49 +0800

    init: spring boot project with mysql and mybatis-plus

---
+++ src/main/java/com/imkeykkk/course/HelloController.java
@@ -0,0 +1,13 @@
+package com.imkeykkk.course;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, I am back.";
+    }
+}
---
+++ src/main/java/com/imkeykkk/course/TeachingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.imkeykkk.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachingSystemApplication.class, args);
+	}
+
+}
